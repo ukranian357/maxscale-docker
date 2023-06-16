a5808ccc971aaf7ce8459502b0d0f6dcfe9ea220
ukranian357 <116136636+ukranian357@users.noreply.github.com>
2023-06-16 13:30:59 -0700

    added req.txt and docker python pip

---
+++ maxscale/sql/primary1/users.sql
@@ -0,0 +1,5 @@
+CREATE USER 'maxuser'@'127.0.0.1' IDENTIFIED BY 'maxpwd';
+GRANT ALL ON *.* TO 'maxuser'@'127.0.0.1' WITH GRANT OPTION;
+
+CREATE USER 'maxuser'@'%' IDENTIFIED BY 'maxpwd';
+GRANT ALL ON *.* TO 'maxuser'@'%' WITH GRANT OPTION;
