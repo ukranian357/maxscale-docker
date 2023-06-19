91133c08611c246db80f5964a41fdcaa3d199023
ukranian357 <116136636+ukranian357@users.noreply.github.com>
2023-06-18 20:28:19 -0700

    final python code add 6/18

--- maxscale/main.py
+++ maxscale/main.py
@@ -1,18 +1,48 @@
-# This is a sample Python script.
+# Igor Nikolaev
+#CNE 370 Maxscale_Docker Assignment
+#W3Schools SQL was used for helping with remembering and find new SQL solutions along with old reference material from class with christine sutton learning SQL
+# This is a working python Python script to query two sharded databses using Max-Scale Horizontal Load Balancing.
 import mysql.connector
 
-def connect_to_db () :
-    con = mysql.connector.connect(
+db = mysql.connector.connect(
         user = 'maxuser',
         password = 'maxpwd',
         host= '127.0.0.1',
         port = '4000'
     )
-    return con
-
-def query(cursor, query):
-    cursor.execute("SELECT * FROM `zipcodes_two` WHERE State=%s", [Kentucky])
-        query = return cursor.fetchall()
+print("LARGEST ZIPCODE FROM DATABASE ONE SHARD1 ARCHIVE DISPLAYED BELOW")   
+cursor = db.cursor()
+cursor.execute("select max(zipcode) as max_zipcode from zipcodes_one.zipcodes_one;") 
+maxzip = cursor.fetchall()
+for maxzip in maxzip:
+    print(maxzip)
+    
+print("ALL STATES WITH THE STATE KENTUCKY ARE DISPLAYED BELOW")  
+cursor = db.cursor()
+cursor.execute("select * from zipcodes_one.zipcodes_one where State='ky':")
+KYstate = cursor.fetchall()
+for KYstate in KYstate:
+    print(KYstate)
+    
+print("ZIPCODES BETWEEN 40 and 41K DISPLAYED BELOW") 
+cursor = db.cursor()
+cursor.execute("select * from zipcodes_one.zipcodes_one where zipcode between 40000 and 41000;")
+between = cursor.fetchall()
+for between in between:
+    print(between)
+    
+print("ALL WAGES IN THE STATE OF PA RECORDED IN ARCHIVE DISPLAYED BELOW!")    
+cursor = db.cursor()
+cursor.execute("select totalwages from zipcodes_one.zipcodes_one where zipcodes_one where state='pa';")
+pawages = cursor.fetchall()
+for pawages in pawages:
+    print(pawages)
+    
+    
+    
+    
+    
+    
 
 
 
